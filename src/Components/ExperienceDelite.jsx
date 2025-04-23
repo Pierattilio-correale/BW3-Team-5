72921b3bfa5a17bd76b5b3cbab8a60496323977d
Pierattilio-correale <pierattiliocorreale@yahoo.it>
2025-04-23 11:51:36 +0200

    new modal

--- src/Components/ExperienceDelite.jsx
+++ src/Components/ExperienceDelite.jsx
@@ -1,6 +1,5 @@
-import { useEffect } from "react";
-import { fetchArrayExperience } from "../Redux/Action";
-import { fetchArrayAction } from "../Redux/Action";
+import { useEffect, useState } from "react";
+import { fetchArrayExperience, fetchArrayAction } from "../Redux/Action";
 import { useDispatch, useSelector } from "react-redux";
 import {
   Alert,
@@ -9,8 +8,9 @@ import {
   ListGroup,
   ListGroupItem,
   Spinner,
+  Modal,
+  Form,
 } from "react-bootstrap";
-import { useParams } from "react-router-dom";
 
 const pierattiliotoken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODA3NWRkM2Q0NTE4MTAwMTVjZTgzZDQiLCJpYXQiOjE3NDUzMTMyMzYsImV4cCI6MTc0NjUyMjgzNn0.1nb5bTwFZyxSFdHoFu9ITxAAdGeQ6LtV1ZolKHc4D88";
@@ -21,7 +21,19 @@ const ExperienceDelite = function () {
   const isLoading = useSelector((state) => state.company.isLoading);
   const isError = useSelector((state) => state.company.isError);
   const dispatch = useDispatch();
-  const params = useParams();
+  const [show, setShow] = useState(false);
+  const [editFormData, setEditFormData] = useState({
+    role: "",
+    company: "",
+    startDate: "",
+    endDate: "",
+    description: "",
+    area: "",
+  });
+  const [currentExpId, setCurrentExpId] = useState(null);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   useEffect(() => {
     dispatch(
@@ -52,8 +64,36 @@ const ExperienceDelite = function () {
         }
       );
       if (response.ok) {
-        alert("Esperienza Eliminata!");
-        // Ricarica le esperienze dopo aver eliminato una
+        alert("Esperienza eliminata!");
+        dispatch(
+          fetchArrayExperience(
+            `https://striveschool-api.herokuapp.com/api/profile/${profile._id}/experiences`
+          )
+        );
+      } else {
+        throw new Error("Errore nella cancellazione");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleEdit = async (expID) => {
+    try {
+      const response = await fetch(
+        `https://striveschool-api.herokuapp.com/api/profile/${profile._id}/experiences/${expID}`,
+        {
+          method: "PUT",
+          headers: {
+            Authorization: `Bearer ${pierattiliotoken}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(editFormData),
+        }
+      );
+      if (response.ok) {
+        alert("Esperienza aggiornata!");
+        handleClose();
         dispatch(
           fetchArrayExperience(
             `https://striveschool-api.herokuapp.com/api/profile/${profile._id}/experiences`
@@ -66,42 +106,135 @@ const ExperienceDelite = function () {
       console.error(err);
     }
   };
+
   return (
-    <>
-      <Col>
-        <ListGroup>
-          {isLoading && (
-            <div className="d-flex justify-content-center">
-              <Spinner variant="primary" animation="border"></Spinner>
+    <Col>
+      <ListGroup>
+        {isLoading && (
+          <div className="d-flex justify-content-center">
+            <Spinner variant="primary" animation="border" />
+          </div>
+        )}
+        {isError && (
+          <Alert variant="danger">Errore nel recupero della lista</Alert>
+        )}
+        {experienceArray?.map((exp) => (
+          <ListGroupItem
+            key={exp._id}
+            className="d-flex justify-content-between"
+          >
+            <div>
+              <h5>{exp.role}</h5>
+              <p>
+                {exp.company} - {exp.area}
+              </p>
+              <p>{exp.description}</p>
+              <p>Data d'inizio: {exp.startDate}</p>
+              <p>Data fine: {exp.endDate || "In corso"}</p>
             </div>
-          )}
-          {isError && (
-            <Alert variant="danger">Errore nel recupero della lista</Alert>
-          )}
-          {experienceArray?.map((exp) => (
-            <ListGroupItem
-              key={exp._id}
-              className="d-flex justify-content-between"
-            >
-              <div>
-                <h5>{exp.role}</h5>
-                <p>
-                  {exp.company} - {exp.area}
-                </p>
-                <p>{exp.description}</p>
-                <p>data d'inizio {exp.startDate}</p>
-                <p>data fine : {exp.endDate || "In corso"}</p>
-              </div>
-              <div>
-                <Button variant="danger" onClick={() => myDelete(exp._id)}>
-                  <i class="bi bi-trash-fill"></i>
-                </Button>
-              </div>
-            </ListGroupItem>
-          ))}
-        </ListGroup>
-      </Col>
-    </>
+            <div>
+              <a
+                href="#"
+                onClick={() => {
+                  setEditFormData(exp);
+                  setCurrentExpId(exp._id);
+                  handleShow();
+                }}
+                className="text-dark me-4"
+              >
+                <i className="bi bi-pencil" style={{ fontSize: "1.4rem" }}></i>
+              </a>
+            </div>
+          </ListGroupItem>
+        ))}
+      </ListGroup>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modifica esperienza</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Qualifica</Form.Label>
+              <Form.Control
+                type="text"
+                value={editFormData.role}
+                onChange={(e) =>
+                  setEditFormData({ ...editFormData, role: e.target.value })
+                }
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Azienda</Form.Label>
+              <Form.Control
+                type="text"
+                value={editFormData.company}
+                onChange={(e) =>
+                  setEditFormData({ ...editFormData, company: e.target.value })
+                }
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Data Inizio</Form.Label>
+              <Form.Control
+                type="date"
+                value={editFormData.startDate}
+                onChange={(e) =>
+                  setEditFormData({
+                    ...editFormData,
+                    startDate: e.target.value,
+                  })
+                }
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Data Fine</Form.Label>
+              <Form.Control
+                type="date"
+                value={editFormData.endDate}
+                onChange={(e) =>
+                  setEditFormData({ ...editFormData, endDate: e.target.value })
+                }
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Località</Form.Label>
+              <Form.Control
+                type="text"
+                value={editFormData.area}
+                onChange={(e) =>
+                  setEditFormData({ ...editFormData, area: e.target.value })
+                }
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Descrizione</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                value={editFormData.description}
+                onChange={(e) =>
+                  setEditFormData({
+                    ...editFormData,
+                    description: e.target.value,
+                  })
+                }
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={() => myDelete(currentExpId)}>
+            <i className="bi bi-trash-fill"></i> Elimina
+          </Button>
+          <Button variant="primary" onClick={() => handleEdit(currentExpId)}>
+            Salva Modifiche
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </Col>
   );
 };
+
 export default ExperienceDelite;
